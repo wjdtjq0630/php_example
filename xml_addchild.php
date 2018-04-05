2a6f6ad2a381e8ff7232e94f0bc9b680d1c48bea
wjdtjq0630 <wjdtjq0630@gmail.com>
2018-04-05 22:59:33 +0900

    commit

--- xml_addchild.php
+++ xml_addchild.php
@@ -3,18 +3,16 @@
   $albums = array('마지막처럼','붐바야','불장난','휘파람');
 
   $xml = new SimpleXMLElement('<idol/>');
-
   $xml->addChild('name','Blackpink');
 
-  $members_num = sizeof($members);
   foreach($members as $member){
-    $xml->addChild('members',$member);
-  }
-  $albums_num = sizeof($albums);
-  foreach($albums as $album){
-    $xml->addChild('albums',$album);
+    $xml->addChild('member',$member);
   }
 
-  Header('Content-type: text/xml');
+  foreach($albums as $album){
+    $xml->addChild('album',$album);
+  }
+
+  header('Content-type: text/xml charset=utf-8');
   print($xml->asXML());
  ?>
